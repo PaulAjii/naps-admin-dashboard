dca4050cd2d403d3568c9f9747bcf61b96caee94
PaulAjii <paulajii134@gmail.com>
2025-08-28 16:25:23 +0100

    feat: dashboard page view

---
+++ src/views/DashboardPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>This the dashboard of {{ authStore.user.fullName }}</h1>
+</template>
+
+<script setup>
+import useAuthStore from '@/stores/auth'
+
+const authStore = useAuthStore()
+</script>
