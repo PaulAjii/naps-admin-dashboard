48fef13dfdae92e54af3dfedbc9facded115ed20
PaulAjii <paulajii134@gmail.com>
2025-08-28 15:58:23 +0100

    feat: update auth store to include reset password request functionality

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -11,28 +11,30 @@ const apiClient = axios.create({
 
 export const auth = {
   login: (credentials) => apiClient.post('/auth/login', credentials),
-  changePassword: (data) => apiClient.post('/auth/change-password', data),
-  requestReset: (data) => apiClient.post('/auth/reset-password-request', data),
-  handleReset: (data) => apiClient.patch('/auth/reset-password', data),
-}
-
-export const users = {
-  getAll: () => apiClient.get('/users'),
-  getById: (id) => apiClient.get(`/users/${id}`),
+  passwordResetRequest: (credentials) =>
+    apiClient.post(`/auth/reset-password-request`, credentials),
   create: (userData) => apiClient.post('/users', userData),
   update: (id, userData) => apiClient.patch(`/users/${id}`, userData),
   remove: (id) => apiClient.delete(`/users/${id}`),
 }
 
-export const news = {
-  getAll: () => apiClient.get('/news'),
-  getById: (id) => apiClient.get(`/news/${id}`),
-  create: (newsData) => apiClient.post('/news', newsData),
-  update: (id, newsData) => apiClient.patch(`/news/${id}`, newsData),
-  remove: (id) => apiClient.delete(`/news/${id}`),
-}
+// export const users = {
+//   getAll: () => apiClient.get('/users'),
+//   getById: (id) => apiClient.get(`/users/${id}`),
+//   create: (userData) => apiClient.post('/users', userData),
+//   update: (id, userData) => apiClient.patch(`/users/${id}`, userData),
+//   remove: (id) => apiClient.delete(`/users/${id}`),
+// }
 
-export const passwordResetRequests = {
-  getAll: () => apiClient.get('/reset-request'),
-  getById: (id) => apiClient.get(`/reset-request/${id}`),
-}
+// export const news = {
+//   getAll: () => apiClient.get('/news'),
+//   getById: (id) => apiClient.get(`/news/${id}`),
+//   create: (newsData) => apiClient.post('/news', newsData),
+//   update: (id, newsData) => apiClient.patch(`/news/${id}`, newsData),
+//   remove: (id) => apiClient.delete(`/news/${id}`),
+// }
+
+// export const passwordResetRequests = {
+//   getAll: () => apiClient.get('/reset-request'),
+//   getById: (id) => apiClient.get(`/reset-request/${id}`),
+// }
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,21 +1,41 @@
 import { defineStore } from 'pinia'
-// import { ref } from "vue"
 import { useLocalStorage } from '@/composables/storage'
 import { auth } from '@/services/api.services'
+import { ref } from 'vue'
 
-const authStore = defineStore('auth', () => {
-  const user = useLocalStorage('user', null)
+const useAuthStore = defineStore('auth', () => {
+  const user = ref(null)
+  const token = useLocalStorage('token', null)
 
   const login = async (userData) => {
     const response = await auth.login(userData)
-    const { data } = response.data
-    user.value = data.token
+    const { data } = response
+
+    if (data.status === 'error') {
+      return Promise.reject(new Error(data.message))
+    }
+
+    token.value = data.data.token
+    user.value = data.data.user
+    return data.message
+  }
+
+  const resetPasswordRequest = async (userData) => {
+    const response = await auth.passwordResetRequest(userData)
+    const { data } = response
+
+    if (data.status === 'error') {
+      return Promise.reject(new Error(data.message))
+    }
+
+    return data.message
   }
 
   return {
     user,
     login,
+    resetPasswordRequest,
   }
 })
 
-export default authStore
+export default useAuthStore
